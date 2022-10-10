02aede0f12b17d87fdb0b71b479a85de66db0af5
EC2 Default User <ec2-user@ip-172-31-44-80.ec2.internal>
2022-10-10 22:54:37 +0900

    fixed database

--- app/Http/Controllers/VocabularyController.php
+++ app/Http/Controllers/VocabularyController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
-
 use Illuminate\Http\Request;
+
+use Inertia\Inertia;
 
 use App\Models\Vocabulary;
 
 class VocabularyController extends Controller
 {
-    public function index(Vocabulary $vocabulary)
-    {
-        return Inertia::render("Vocabulary/Index",["vocabularies" => $vocabulary->get()]);
-    }
+        public function index(Vocabulary $vocabulary)
+        {
+            return Inertia::render("Vocabulary/Index",["vocabularies" => $vocabulary->get()]);
+        }
 
 }
