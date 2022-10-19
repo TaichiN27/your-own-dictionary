9707964547d1a748991b95da436f4a0b3fe4fdcf
TaichiN27 <ragu.cap@icloud.com>
2022-10-19 10:27:18 +0900

    appserver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       //\URL::forceScheme("https");
+       \URL::forceScheme("https");
        $this->app['request']->server->set('HTTPS','on');
        
        Paginator::useBootstrap();  
