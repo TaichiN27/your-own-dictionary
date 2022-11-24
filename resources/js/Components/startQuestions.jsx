0190feeba60a29c8329c724b82598214663a0866
TaichiN27 <ragu.cap@icloud.com>
2022-11-24 15:25:53 +0900

    disabled the button of start the quiz

--- resources/js/Components/startQuestions.jsx
+++ resources/js/Components/startQuestions.jsx
@@ -3,8 +3,16 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 
-export const StartQuestions = ({ currentSituation, setCurrentSituation, correctAns }) => {
 
+
+
+export const StartQuestions = ({data, currentSituation, setCurrentSituation, correctAns }) => {
+    let notstart = false
+    if (data.length<3) {
+        notstart = true
+    } else {
+        notstart = false
+    }
     function start() {
         const music = new Audio('/audios/Quiz-Question01-1.mp3');
         music.play();
@@ -20,9 +28,17 @@ export const StartQuestions = ({ currentSituation, setCurrentSituation, correctA
                         <h1 className="text-white display-4">Let's challenge the quiz!!</h1>
                     </div>
                     <div className="text-center mt-20">
-                        <Button variant="contained" onClick={start} sx={{ width: '30%', height: '100px' }}>
+                        {notstart ?
+                            <div>
+                                <h1 className="text-white display-4">Sorry, you need more than 3 words on your dictionary in order to start the quiz...</h1>
+                                <Button disabled variant="contained" onClick={start} sx={{ width: '30%', height: '100px' }}>Start</Button>
+                            </div>
+                            :
+                            <Button variant="contained" onClick={start} sx={{ width: '30%', height: '100px' }}>
                             Start
-                        </Button>
+                             </Button>
+                        }
+
                     </div>
                 </div>
             </div>
