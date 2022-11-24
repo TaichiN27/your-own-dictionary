64af5e750fb228a53eda5f4fa01b2965c56dbd03
TaichiN27 <ragu.cap@icloud.com>
2022-11-24 15:43:55 +0900

    console check

--- resources/js/Components/startQuestions.jsx
+++ resources/js/Components/startQuestions.jsx
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 
 export const StartQuestions = ({data, currentSituation, setCurrentSituation, correctAns }) => {
     let notstart = false
+    console.log(data.length);
     if (data.length<3) {
         notstart = true
     } else {
